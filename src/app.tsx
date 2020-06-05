9ca72f406f39af60754a14e9a05f5bab7fe62909
Cinnamon <cinnamon_bun_github@fastmail.com>
2020-06-04 17:37:16 -0700

    demo code for react-router

--- src/app.tsx
+++ src/app.tsx
@@ -4,7 +4,9 @@ import {
     BrowserRouter as Router,
     Switch,
     Route,
-    Link
+    Link,
+    NavLink,
+    useParams,
 } from "react-router-dom";
 
 import {
@@ -30,40 +32,105 @@ import { WikiView } from './wikiView';
 //================================================================================
 // REACT ROUTER EXAMPLE
 
-class ReactRouterExample extends React.Component<any, any> {
-    render() {
-        return <Router>
-            <Card>
-                <Stack>
-                    <div><Link to="/">Home</Link></div>
-                    <div><Link to="/foo">Foo</Link></div>
-                    <div><Link to="/bar">Bar</Link></div>
-                </Stack>
-            </Card>
+const ReactRouterExample : React.FunctionComponent = (props) =>
+    <Router>
+        <Card>
+            <Stack>
+                <div><NavLink exact to="/">Home</NavLink></div>
+                <div><NavLink exact to="/login">Login</NavLink></div>
+                <div><NavLink exact to="/ws/test/about">List of authors</NavLink></div>
+                <div><NavLink exact to="/ws/test/about/@abc">About one author</NavLink></div>
+                <div><NavLink exact to="/ws/test/wiki">Wiki frontpage</NavLink></div>
+                <div><NavLink exact to="/ws/test/wiki/recent">Wiki recent</NavLink></div>
+                <div><NavLink exact to="/ws/test/wiki/page/Dogs">Wiki: Dogs</NavLink></div>
+                <div><NavLink exact to="/ws/test/wiki/page/Cats">Wiki: Cats</NavLink></div>
+                <div><NavLink exact to="/ws/test/wiki/page/Dogs And Cats">Wiki: Dogs And Cats</NavLink></div>
+            </Stack>
+        </Card>
 
-            <hr />
+        <hr />
 
-            <Switch>
-                <Route path="/">
-                    <CardExample text={'home'} />
-                </Route>
-                <Route path="/foo">
-                    <CardExample text={'foo'} />
-                </Route>
-                <Route path="/bar">
-                    <CardExample text={'bar'} />
-                </Route>
-            </Switch>
-        </Router>;
-    }
-}
+        <Switch>
+            <Route exact path='/'>
+                <CardExample text={'home'} />
+            </Route>
+            <Route exact path='/login'>
+                <CardExample text={'login'} />
+            </Route>
+            <Route exact path='/ws/:workspace/about'>
+                <AboutFrontpage />
+            </Route>
+            <Route exact path='/ws/:workspace/about/:author'>
+                <AboutAuthor />
+            </Route>
+            <Route exact path='/ws/:workspace/wiki'>
+                <WikiFrontpage />
+            </Route>
+            <Route exact path='/ws/:workspace/wiki/recent'>
+                <WikiRecent />
+            </Route>
+            <Route exact path='/ws/:workspace/wiki/page/:title'>
+                <WikiPage />
+            </Route>
+            <Route path='*'>
+                <FourOhFour />
+            </Route>
+        </Switch>
+    </Router>
 
-interface CardExampleProps {
-    text : string;
-};
-const CardExample : React.FunctionComponent<CardExampleProps> = (props) =>
+const FourOhFour : React.FunctionComponent = (props) =>
+    <h3>404</h3>
+
+const CardExample : React.FunctionComponent<{ text : string }> = (props) =>
     <Card>{props.text}</Card>;
 
+const AboutFrontpage : React.FunctionComponent = (props) =>
+    <Card>
+        <h3>List of authors</h3>
+    </Card>;
+
+const AboutAuthor : React.FunctionComponent = (props) => {
+    let { author } = useParams();
+    return <Card>
+        <h3>{author}</h3>
+    </Card>;
+}
+
+const WikiFrontpage : React.FunctionComponent = (props) => {
+    let { workspace, title } = useParams();
+    let pages = ['Dogs', 'Cats', 'Dogs And Cats'];
+    return <Card>
+        <p><i>Workspace: <code>{workspace}</code></i></p>
+        <h3>List of pages</h3>
+        {pages.map(page =>
+            <p key={page}><Link to={`/ws/${workspace}/wiki/page/${page}`}>{page}</Link></p>
+        )}
+    </Card>;
+}
+
+const WikiRecent : React.FunctionComponent = (props) =>
+    <Card>
+        <h3>Recent wiki pages</h3>
+    </Card>;
+
+const WikiPage : React.FunctionComponent = (props) => {
+    let { workspace, title } = useParams();
+    return <Card>
+        <p><i>Workspace: <code>{workspace}</code></i></p>
+        <h3>{title}</h3>
+        <p>blah blah</p>
+    </Card>;
+}
+
+//================================================================================
+// MAIN
+
+ReactDOM.render(
+    <ReactRouterExample />,
+    document.getElementById('react-slot')
+);
+
+/*
 //================================================================================
 // APP VIEW
 
@@ -166,6 +233,7 @@ syncer.addPub('http://167.71.153.73:3333/earthstar/');
 
 ReactDOM.render(
     <AppView es={es} keypair={demoKeypair} syncer={syncer} />,
-    //<ReactRouterExample />,
     document.getElementById('react-slot')
 );
+
+*/
