6c431ccfbf84464d3e0c801295298b23feb19cf9
Cinnamon <cinnamon_bun_github@fastmail.com>
2020-06-15 20:09:02 -0700

    new page button works

--- src/views/wikiPageList.tsx
+++ src/views/wikiPageList.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import {
     Link,
     useParams,
+    Redirect,
 } from "react-router-dom";
 import {
     AboutLayer,
@@ -53,18 +54,55 @@ export class FetchWikiPageList extends React.Component<BasicProps> {
         logDisplayPageList('render()');
         // HACK: for now, limit to shared pages
         let pageInfos = this.props.wikiLayer.listPageInfos('shared');
-        return <WikiPageList workspace={this.props.storage.workspace} pageInfos={pageInfos} />;
+        return <WikiPageList
+            workspace={this.props.storage.workspace}
+            pageInfos={pageInfos}
+            wikiLayer={this.props.wikiLayer}
+            />;
     }
 }
+
 
 interface WikiPageListProps {
     workspace : WorkspaceAddress,
     pageInfos : WikiPageInfo[],
+    wikiLayer : WikiLayer,
 }
-export const WikiPageList : React.FunctionComponent<WikiPageListProps> = (props) =>
-    <Stack>
-        <h3>All shared pages</h3>
-        {props.pageInfos.map((pageInfo : WikiPageInfo) =>
-            <p key={pageInfo.path}><Link to={Urls.wiki(props.workspace, pageInfo.path)}>{pageInfo.title}</Link></p>
-        )}
-    </Stack>
+interface WikiPageListState {
+    redirectTo : string | null;
+}
+export class WikiPageList extends React.Component<WikiPageListProps, WikiPageListState> {
+    constructor(props : WikiPageListProps) {
+        super(props);
+        this.state = { redirectTo: null }
+    }
+    _newPage() {
+        logDisplayPageList('makeNewPage');
+        let title = window.prompt('Page title');
+        if (!title) { return; }
+        let path = WikiLayer.makePagePath('shared', title);  // TODO: allow making personal pages too, not just shared
+        let ok = this.props.wikiLayer.setPageText(path, '...');
+        if (ok) {
+            let newUrl = Urls.wiki(this.props.wikiLayer.storage.workspace, path);
+            this.setState({ redirectTo: newUrl });
+        }
+    }
+    render() {
+        if (this.state.redirectTo) {
+            return <Redirect to={this.state.redirectTo}/>
+        }
+        return <Stack>
+            {logDisplayPageList('render')}
+            <button type="button"
+                style={{float: 'right', marginLeft: 10}}
+                onClick={() => this._newPage()}
+                >
+                New Page
+            </button>
+            <h3>All shared pages</h3>
+            {this.props.pageInfos.map((pageInfo : WikiPageInfo) =>
+                <p key={pageInfo.path}><Link to={Urls.wiki(this.props.workspace, pageInfo.path)}>{pageInfo.title}</Link></p>
+            )}
+        </Stack>
+    }
+}
